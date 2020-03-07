65ff53051ffd7640938a72be67900dcad586080e
doofmars <jan-preuss@gmx.de>
2020-03-07 19:52:27 +0100

    Reindent file

--- js/gamemaster.js
+++ js/gamemaster.js
@@ -6,46 +6,46 @@
    * Obtains parameters from the hash of the URL
    * @return Object
    */
-   function getHashParams() {
-     var hashParams = {};
-     var e, r = /([^&;=]+)=?([^&;]*)/g,
-     q = window.location.hash.substring(1);
-     history.replaceState(null, null, ' ');
-     while ( e = r.exec(q)) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
-     }
-     return hashParams;
-   }
+  function getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    history.replaceState(null, null, ' ');
+    while (e = r.exec(q)) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
 
-   userProfileTemplate = Handlebars.templates.playlists,
-   userProfilePlaceholder = document.getElementById('user-profile');
+  userProfileTemplate = Handlebars.templates.playlists,
+    userProfilePlaceholder = document.getElementById('user-profile');
 
-   var params = getHashParams();
+  var params = getHashParams();
 
-   var access_token = params.access_token,
-   state = params.state,
-   storedState = localStorage.getItem(stateKey);
+  var access_token = params.access_token,
+    state = params.state,
+    storedState = localStorage.getItem(stateKey);
 
 
-   if (access_token && (state === null || state !== storedState)) {
-     alert('There was an error during the authentication');
-   } else {
-     localStorage.removeItem(stateKey);
-     if (access_token) {
-       $.ajax({
-         url: 'https://api.spotify.com/v1/me',
-         headers: {
-           'Authorization': 'Bearer ' + access_token
-         },
-         success: function(response) {
-           userProfilePlaceholder.innerHTML = userProfileTemplate(response);
-           $('#login').hide();
-           $('#loggedin').show();
-         }
-       });
-     } else {
-       $('#login').show();
-       $('#loggedin').hide();
-     }
-   }
+  if (access_token && (state === null || state !== storedState)) {
+    alert('There was an error during the authentication');
+  } else {
+    localStorage.removeItem(stateKey);
+    if (access_token) {
+      $.ajax({
+        url: 'https://api.spotify.com/v1/me',
+        headers: {
+          'Authorization': 'Bearer ' + access_token
+        },
+        success: function(response) {
+          userProfilePlaceholder.innerHTML = userProfileTemplate(response);
+          $('#login').hide();
+          $('#loggedin').show();
+        }
+      });
+    } else {
+      $('#login').show();
+      $('#loggedin').hide();
+    }
+  }
 })();
