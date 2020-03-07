2f98ef690f964f75eabc0becf7c8a8e21880ed64
doofmars <jan-preuss@gmx.de>
2020-03-07 12:56:04 +0100

    Create landing page for game

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ var app = require('express')();
 var http = require('http').createServer(app);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/style.css', function(req, res) {
+  res.sendFile(__dirname + "/" + "style.css");
+});
+
+app.get('/jquery-3.4.1.min.js', function(req, res) {
+  res.sendFile(__dirname + "/" + "jquery-3.4.1.min.js");
 });
 
 http.listen(3000, function(){
