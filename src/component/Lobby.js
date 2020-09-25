f08e44be0c26ef8433668bca00af3bc3c9122946
doofmars <jan-preuss@gmx.de>
2020-09-25 19:01:28 +0200

    Fix issues in css
    Get layout working

--- src/component/Lobby.js
+++ src/component/Lobby.js
@@ -8,29 +8,31 @@ export default class Lobby extends React.Component {
 
   render() {
     return (
-      <div className="Lobby">
-        <h1 class="cyan mb-3 block">Spotiguess</h1>
-        <div id="gamestart">
-          <button class="sbtn sbtn-green mb-1 btn-block" id="login-button" type="button">
-            Create new game
-          </button>
-          <p class="text-white mb-4">
-            Login using your spoitify account,
-            to access your playlists
-          </p>
-          <button class="sbtn sbtn-white mb-1 btn-block" id="join-init" type="button">
-            Join Game
-          </button>
-          <p class="text-white">
-            Join an active session
-          </p>
-          <div id="join-info">
-            <input id="name" type="text" class="form-control mb-1" placeholder="Name" />
-            <input id="room-code" type="text" class="form-control mb-1" maxlength="5" placeholder="Roomkey" />
-            <button class="sbtn sbtn-green mb-1 float-right" id="join-accept" type="button">
-              Join
+      <div class="content-start">
+        <div className="Lobby">
+          <h1 class="cyan mb-3 block">Spotiguess</h1>
+          <div id="gamestart">
+            <button class="sbtn sbtn-green mb-1 btn-block" id="login-button" type="button">
+              Create new game
             </button>
-            <p id="bad-message" class="text-danger"></p>
+            <p class="text-white mb-4">
+              Login using your spoitify account,
+              to access your playlists
+            </p>
+            <button class="sbtn sbtn-white mb-1 btn-block" id="join-init" type="button">
+              Join Game
+            </button>
+            <p class="text-white">
+              Join an active session
+            </p>
+            <div id="join-info">
+              <input id="name" type="text" class="form-control mb-1" placeholder="Name" />
+              <input id="room-code" type="text" class="form-control mb-1" maxlength="5" placeholder="Roomkey" />
+              <button class="sbtn sbtn-green mb-1 float-right" id="join-accept" type="button">
+                Join
+              </button>
+              <p id="bad-message" class="text-danger"></p>
+            </div>
           </div>
         </div>
       </div>
