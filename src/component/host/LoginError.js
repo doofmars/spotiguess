ffe0f10a218cd8f450c55432e2592f3a70dbebdf
doofmars <jan-preuss@gmx.de>
2020-09-27 19:32:15 +0200

    Enable retry button on error component

--- src/component/host/LoginError.js
+++ src/component/host/LoginError.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import login from "./../logic/login";
+
 
 export default class LoginError extends React.Component {
+
+  retryClick() {
+    login();
+  }
+
   render() {
     return (
       <div id="login">
@@ -8,7 +15,7 @@ export default class LoginError extends React.Component {
           <small className="text-muted"> Login to start a game</small>
         </h1>
         <p className="text-danger" id="error">There was an error during the authentication</p>
-        <button id="login-button" className="sbtn sbtn-green mb-1">Log in with Spotify</button>
+        <button id="login-button" className="sbtn sbtn-green mb-1" onClick={this.retryClick}>Log in with Spotify</button>
         <p>
         <button id="back-button" className="sbtn sbtn-white mb-1" onClick={this.props.viewChangeEvent.bind(this, 'lobby')}>Back to lobby</button>
         </p>
