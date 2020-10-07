863a8a7f84a373255d549a90920152e00e3252d6
doofmars <jan-preuss@gmx.de>
2020-10-07 22:12:54 +0200

    Show playlist selection if none has been selected yet
    Improve error message

--- src/component/host/CreateGame.js
+++ src/component/host/CreateGame.js
@@ -46,6 +46,7 @@ export default class CreateGame extends React.Component {
   shuffleClick = () => {
     if (this.context.state.selectedPlaylistId === "") {
       this.setState({warning: true});
+      this.setShowConfig(false)
     } else {
       getPlaylist(
         this.context.state.selectedPlaylistId,
@@ -60,8 +61,8 @@ export default class CreateGame extends React.Component {
     }
   }
 
-  configToggle = () => {
-    this.setState({showConfig: !this.state.showConfig});
+  setShowConfig = (value) => {
+    this.setState({showConfig: value});
   }
 
   joinRequest = (msg) => {
@@ -95,7 +96,7 @@ export default class CreateGame extends React.Component {
         joinedPlayer.push(<JoinedPlayer name={"No one has joined yet"} key={0}/>)
     } else {
       this.context.state.players.forEach((value, key) => {
-        joinedPlayer.push(<JoinedPlayer name={key} key={key}/>)
+        joinedPlayer.push(<JoinedPlayer name={key} key={key} hasVoted={false}/>)
       });
     }
 
@@ -115,10 +116,10 @@ export default class CreateGame extends React.Component {
                 <button className="sbtn sbtn-green mb-1 float-right" id="shuffle" onClick={this.shuffleClick}>
                   Shuffle Play
                 </button>
-                <button className="sbtn sbtn-gray mb-1 float-right gear" onClick={this.configToggle}>
+                <button className="sbtn sbtn-gray mb-1 float-right gear" onClick={() => this.setShowConfig(!this.state.showConfig)}>
                   <svg xmlns="http://www.w3.org/2000/svg" width="12px" height="12px" viewBox="0 0 24 24"><path d="M24 13.616v-3.232c-1.651-.587-2.694-.752-3.219-2.019v-.001c-.527-1.271.1-2.134.847-3.707l-2.285-2.285c-1.561.742-2.433 1.375-3.707.847h-.001c-1.269-.526-1.435-1.576-2.019-3.219h-3.232c-.582 1.635-.749 2.692-2.019 3.219h-.001c-1.271.528-2.132-.098-3.707-.847l-2.285 2.285c.745 1.568 1.375 2.434.847 3.707-.527 1.271-1.584 1.438-3.219 2.02v3.232c1.632.58 2.692.749 3.219 2.019.53 1.282-.114 2.166-.847 3.707l2.285 2.286c1.562-.743 2.434-1.375 3.707-.847h.001c1.27.526 1.436 1.579 2.019 3.219h3.232c.582-1.636.75-2.69 2.027-3.222h.001c1.262-.524 2.12.101 3.698.851l2.285-2.286c-.744-1.563-1.375-2.433-.848-3.706.527-1.271 1.588-1.44 3.221-2.021zm-12 2.384c-2.209 0-4-1.791-4-4s1.791-4 4-4 4 1.791 4 4-1.791 4-4 4z"/></svg>
                 </button>
-                <span className="text-danger float-right" style={this.state.warning ? null : {display: 'none'} } id="error-shuffle">Failed to start shuffle</span>
+                <span className="text-danger float-right" style={this.state.warning ? null : {display: 'none'} } id="error-shuffle">Failed to start shuffle, please select a playlist</span>
               </div>
             </div>
           </div>
