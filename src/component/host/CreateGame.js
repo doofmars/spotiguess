c2cf92c80d0a36bab69b18100bdfd2e0a75c7927
doofmars <jan-preuss@gmx.de>
2020-10-07 21:04:59 +0200

    Add toggle button for config

--- src/component/host/CreateGame.js
+++ src/component/host/CreateGame.js
@@ -19,6 +19,7 @@ export default class CreateGame extends React.Component {
     super(props);
     this.state = {
       warning: false,
+      showConfig: false,
       playlists: {items:[]}
     };
   }
@@ -58,6 +59,10 @@ export default class CreateGame extends React.Component {
     }
   }
 
+  configToggle = () => {
+    this.setState({showConfig: !this.state.showConfig});
+  }
+
   joinRequest = (msg) => {
     socket.emit('join-accepted', msg);
     if (this.context.state.players.get(msg.name)) {
@@ -74,9 +79,19 @@ export default class CreateGame extends React.Component {
 
   render() {
     const joinedPlayer = [];
+    let title;
+    let view;
+
+    if (this.state.showConfig) {
+      title = "Configure game"
+      view = <HostContext.Consumer>{(context) => (<Config context={context} />)}</HostContext.Consumer>
+    } else {
+      title = "Select a collaborative playlist";
+      view = <PlaylistTable playlists={this.state.playlists} />
+    }
 
     if (this.context.state.players.size === 0) {
-        joinedPlayer.push(<JoinedPlayer name={"No one has joined yet"}/>)
+        joinedPlayer.push(<JoinedPlayer name={"No one has joined yet"} key={0}/>)
     } else {
       this.context.state.players.forEach((value, key) => {
         joinedPlayer.push(<JoinedPlayer name={key} key={key}/>)
@@ -87,8 +102,8 @@ export default class CreateGame extends React.Component {
       <div className="container">
         <div id="create">
           <div id="playlists">
-            <h1 className="cyan mb-3">Select a collaborative playlist</h1>
-            <PlaylistTable playlists={this.state.playlists} />
+            <h1 className="cyan mb-3">{title}</h1>
+            {view}
           </div>
           <div className="container">
             <div className="row">
@@ -99,7 +114,15 @@ export default class CreateGame extends React.Component {
                 <button className="sbtn sbtn-green mb-1 float-right" id="shuffle" onClick={this.shuffleClick}>
                   Shuffle Play
                 </button>
-                <span className="text-danger align-middle" style={this.state.warning ? null : {display: 'none'} } id="error-shuffle">Failed to start shuffle</span>
+                <button className="sbtn sbtn-gray mb-1 float-right gear" onClick={this.configToggle}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="12px" height="12px" viewBox="0 0 24 24"><path d="M24 13.616v-3.232c-1.651-.587-2.694-.752-3.219-2.019v-.001c-.527-1.271.1-2.134.847-3.707l-2.285-2.285c-1.561.742-2.433 1.375-3.707.847h-.001c-1.269-.526-1.435-1.576-2.019-3.219h-3.232c-.582 1.635-.749 2.692-2.019 3.219h-.001c-1.271.528-2.132-.098-3.707-.847l-2.285 2.285c.745 1.568 1.375 2.434.847 3.707-.527 1.271-1.584 1.438-3.219 2.02v3.232c1.632.58 2.692.749 3.219 2.019.53 1.282-.114 2.166-.847 3.707l2.285 2.286c1.562-.743 2.434-1.375 3.707-.847h.001c1.27.526 1.436 1.579 2.019 3.219h3.232c.582-1.636.75-2.69 2.027-3.222h.001c1.262-.524 2.12.101 3.698.851l2.285-2.286c-.744-1.563-1.375-2.433-.848-3.706.527-1.271 1.588-1.44 3.221-2.021zm-12 2.384c-2.209 0-4-1.791-4-4s1.791-4 4-4 4 1.791 4 4-1.791 4-4 4z"/></svg>
+                </button>
+                <span className="text-danger float-right" style={this.state.warning ? null : {display: 'none'} } id="error-shuffle">Failed to start shuffle</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
               </div>
             </div>
           </div>
